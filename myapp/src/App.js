a1724f79e6834e138a007b7237c5a1869059b4c1
Nevin1901 <nevinpuri1901@gmail.com>
2022-07-30 20:50:54 +0200

    add react app

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import { init, WASI } from "@wasmer/wasi";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {}, []);
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
