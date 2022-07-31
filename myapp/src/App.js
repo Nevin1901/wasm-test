3a9d80f06c71523dfc8345599a28fcb7002d5b9f
Nevin1901 <nevinpuri1901@gmail.com>
2022-07-31 11:25:28 +0200

    wasi update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,5 @@
 import { init, WASI } from "@wasmer/wasi";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Buffer } from "buffer";
 
 function App() {
@@ -7,22 +7,45 @@ function App() {
     const myFun = async () => {
       window.Buffer = Buffer;
       await init();
+      let w = new WASI();
       let wasi = new WASI({ env: {}, args: [] });
 
       let bytes = fetch("http://localhost:3001/wasm-test.wasm");
       let module = await WebAssembly.compileStreaming(bytes);
 
       await wasi.instantiate(module);
+      wasi.setStdinString("hello");
       let exitCode = wasi.start();
 
-      console.log(wasi.getStdoutString());
+      // setOutput(output + "\n" + wasi.getStdoutString());
+      setInterval(() => {
+        let ostring = wasi.getStdoutString();
+        console.log(ostring);
+        if (ostring != undefined && output != null && ostring != "") {
+          setOutput(output + "\n" + ostring);
+        }
+      }, 100);
+
+      // console.log(wasi.getStdoutString());
     };
 
     myFun();
   }, []);
+  const [wasi, setWasi] = useState();
+  const [output, setOutput] = useState();
+  const [input, setInput] = useState();
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Output</h1>
+      <textarea value={output} />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input onChange={(e) => setInput(e.target.value)} />
+        <button type="submit"></button>
+      </form>
     </div>
   );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io::stdin;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut input_str = String::new();
+    stdin().read_line(&mut input_str).unwrap();
+    println!("{}", input_str);
 }
